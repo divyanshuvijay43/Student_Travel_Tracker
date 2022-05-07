42106d1606428be566994317788fcb595d3ba3e9
Divyanshu Vijayvergiya <41202337+divyanshuvijay43@users.noreply.github.com>
2022-05-07 18:17:22 +0530

    Removed java files from master and created a branch

--- src/main/java/com/spe/project/Repository/StudentRepo.java
+++
@@ -1,7 +0,0 @@
-package com.spe.project.Repository;
-
-import com.spe.project.Model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepo extends JpaRepository<Student, String> {
-}
--- src/main/java/com/spe/project/Repository/TravelRepo.java
+++
@@ -1,7 +0,0 @@
-package com.spe.project.Repository;
-
-import com.spe.project.Model.Travel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TravelRepo extends JpaRepository<Travel, Long> {
-}
