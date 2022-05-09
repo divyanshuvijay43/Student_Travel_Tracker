18a7bfe5df2dee8d164853ebc9c3da62041d51ca
Divyanshu Vijayvergiya <41202337+divyanshuvijay43@users.noreply.github.com>
2022-05-10 00:04:43 +0530

    Add files via upload

---
+++ .github/workflows/ansible/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+echo "Ansible Entrypoint"
+
+echo "[azure]" >> /hosts
+echo "52.140.49.81" >> /hosts
+
+echo "[all:vars]" >> /hosts
+echo "ansible_connection=ssh" >> /hosts
+echo "ansible_user= $SSH_USER" >> /hosts
+echo "ansible_ssh_user= $SSH_USER" >> /hosts
+echo "ansible_python_interpreter=/usr/bin/python3" >> /hosts
+echo "ansible_ssh_private_key_file=/studenttravelvm_key.pem" >> /hosts
+
+echo "Entering the ansible using ansible-playbook"
+
+ansible-playbook playbook.yml --user $SSH_USER
