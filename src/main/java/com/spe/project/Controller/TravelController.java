327d91d747064d895617c1df1787a646aee40d8f
pantprakhar04 <prakhar.sag22@outlook.com>
2022-05-10 21:56:50 +0530

    bug fixes

--- src/main/java/com/spe/project/Controller/TravelController.java
+++ src/main/java/com/spe/project/Controller/TravelController.java
@@ -5,10 +5,7 @@ import com.spe.project.Model.Travel;
 import com.spe.project.Repository.StudentRepo;
 import com.spe.project.Repository.TravelRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
