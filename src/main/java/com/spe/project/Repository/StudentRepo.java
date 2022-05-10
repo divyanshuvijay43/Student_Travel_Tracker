ee21c09e1ad41366eae29a74ef2d3e1bad1e2a1d
Prakhar Pant <prakhar.sag22@outlook.com>
2022-05-10 21:15:10 +0530

    find student by his login details

--- src/main/java/com/spe/project/Repository/StudentRepo.java
+++ src/main/java/com/spe/project/Repository/StudentRepo.java
@@ -2,6 +2,10 @@ package com.spe.project.Repository;
 
 import com.spe.project.Model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface StudentRepo extends JpaRepository<Student, String> {
+  
+  @Query("SELECT s FROM Student s WHERE s.id=:id AND s.password=:pwd")
+  Student findByUsername(String id, String pwd);
 }
